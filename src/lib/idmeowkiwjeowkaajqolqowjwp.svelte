f6cae923cc4a0d2a56e46754013710c0e3d473fd
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-09-20 07:06:49 +0000

    fixed blogwrtie

--- src/lib/idmeowkiwjeowkaajqolqowjwp.svelte
+++ src/lib/idmeowkiwjeowkaajqolqowjwp.svelte
@@ -1,23 +1,24 @@
 <script>
-
+let code;
+  let password;
 console.log("hii")
     function submitHandler(){
     console.log("Handling submit")
     event.preventDefault()
     const form = event.target;
-    let code = form.getElementById("lcode").getElementById("code").input; 
-    let password = form.getElementById("lpass").getElementById("pass").input;
-    if (password = "rami.com123"){ 
-   console.log(passkey);
-    pass = "correct!"
-        } 
+    
+      console.log(password)
+   // if (password = "ramicom"){ 
+  //  this.pass = "correct!"
+     // console.log("hello")
+     //   } 
 }
 </script>
 
 <form on:submit={submitHandler}>
   <label for="code" id="lcode">Code:</label>
-  <textarea id="code" name="code" rows="5" cols="100"></textarea><br>
+  <textarea id="code" name="code" rows="5" cols="100" bind:this={code}></textarea><br>
   <label for="pass" id="lpass">Password:</label>
-  <input type="text" id="pass" name="pass"><br><br>
+  <input type="text" id="pass" name="pass" bind:this={password}><br><br>
   <input type="submit" value="Submit">
 </form>
