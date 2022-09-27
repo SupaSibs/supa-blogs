94283a5ece6b29953cb1d89b219510e1b5113dd8
SupaSibs <92636078+SupaSibs@users.noreply.github.com>
2022-09-27 07:07:58 +0000

    Changed stuff

--- src/lib/giveaways.svelte
+++ src/lib/giveaways.svelte
@@ -4,5 +4,15 @@
     <h1>
 Supa's Giveaways
     </h1>
+  <select name="country" id="country-select">
+    <option value="">--Please choose an option--</option>
+    <option value="rf">--DE (Germany)--</option>
+    <option value="us">--US (United States)--</option>
+    <option value="uk">--UK (United Kingdom)--</option>
+    <option value="jo">--JO (Jordan)--</option>
+    <option value="">--IN (India)--</option>
+    <option value="">--FR (France)--</option>
+    <option value="">--IT( (Italy)--</option>
+</select>
 </main>
     
